9d4bd0f9ebd056a9be4416d0bbcf773dcafa39b6
Rodron <your_email@gmail.com>
2026-07-03 23:52:08 +0800

    Fix Railway deployment

--- app.py
+++ app.py
@@ -1199,6 +1199,5 @@ def server_error(error):
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=5000,
-        debug=True
+        port=int(os.environ.get("PORT", 5000))
     )
